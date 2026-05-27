71d491daefe0a73450c701cd90577ecae43ccb6f
Enrique Medina <125900781+LM-Enrique@users.noreply.github.com>
2026-05-27 12:10:00 +0200

    [FIX] index.html & icon.png

--- analytic_distribution_team/__manifest__.py
+++ analytic_distribution_team/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Analytic Distribution by Sales Team',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Accounting/Accounting',
     'summary': 'Add Sales Team as a matching criterion for analytic '
                'distribution models.',
